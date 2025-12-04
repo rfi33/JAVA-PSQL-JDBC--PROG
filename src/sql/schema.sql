f782c16292608746081088b771546816b9896335
Finoana <hei.finoana.5@gmail.com>
2025-12-05 00:21:39 +0300

    feat(database): create product_management_db and tables Product & Product_category

---
+++ src/sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Product (
+id SERIAL PRIMARY KEY,
+name VARCHAR (100),
+price NUMBER ,
+creation_datetime TIMESTAMP
+);
+
+CREATE TABLE Product_category (
+id SERIAL PRIMARY KEY ,
+name VARCHAR (50),
+product_id int REFERENCES Product(id)
+);
