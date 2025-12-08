f065216d77a723b3ef75246d52d0942f8b31a554
Finoana <hei.finoana.5@gmail.com>
2025-12-08 19:36:17 +0300

    fix(database): fix connection to product_management_db

--- src/sql/init_db.sql
+++ src/sql/init_db.sql
@@ -1,26 +1,13 @@
-createuser --interactive product_management_user
-Shall the new role be a superuser? (y/n) n
-Shall the new role be allowed to create databases? (y/n) y
-Shall the new role be allowed to create more new roles? (y/n) n
-postgres@pop-os:~$ psql
-
-postgres=# \password product_manager_user
-Enter new password for user "product_management_user": 123456
-Enter it again: 123456
-
-CREATE DATABASE product_management_db OWNER product_manager_user;
-\q
-psql -U product_manager_user -d product_management_db  -h localhost
-
-
 CREATE USER product_manager_user WITH
 NOSUPERUSER
 CREATEDB
 NOCREATEROLE
-ENCRYPTED PASSWORD '123456';
+PASSWORD '123456';
 
 CREATE DATABASE product_management_db OWNER product_manager_user;
-\q
-psql -U product_manager_user -d product_management_db -h localhost
+\c product_management_db product_manager_user;
 
 
+CREATE DATABASE product_management_db OWNER product_manager_user;
+\c product_management_db;
+SET ROLE product_manager_user;
